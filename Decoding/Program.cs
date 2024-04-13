67b51432ba1b804d182f139cf8ec91e92a7a1483
unknown <islammohamedahmed49@gmail.com>
2024-04-13 13:37:35 +0200

    d

---
+++ Decoding/Program.cs
@@ -0,0 +1,31 @@
+﻿using static System.Console;
+using static System.Convert;
+using static System.Math;
+
+
+int n;
+string str,testCase,res="";
+
+
+n = ToInt32(ReadLine());
+str = ReadLine();
+
+testCase = str;
+
+
+while(testCase.Length > 0)
+{
+    if(testCase.Length % 2 == 0)
+    {
+        res = testCase[0] + res;
+    }
+    else
+    {
+        res += testCase[0];
+
+    }
+    testCase =testCase.Remove(0,1);
+}
+
+
+WriteLine(res);
